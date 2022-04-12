f598a614bfed3cb9362f162693fb1d72efbb5f73
Antonette Caldwell <pullmana8@gmail.com>
2022-04-11 22:13:58 -0500

    Completed second part of tutorial

--- index.mjs
+++ index.mjs
@@ -9,11 +9,24 @@ const accBob = await stdlib.newTestAccount(startingBalance);
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+const HAND = ['Rock', 'Paper', 'Scissor'];
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+const Player = (Who) => ({
+    getHand: () => {
+        const hand = Math.floor(Math.random() * 3);
+        console.log(`${Who} played ${HAND[hand]}`);
+        return hand;
+    },
+    seeOutcome: (outcome) => {
+        console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+    },
+});
+
 await Promise.all([
     ctcAlice.p.Alice({
-
+        ...Player('Alice'),
     }),
     ctcBob.p.Bob({
-
+        ...Player('Bob')
     }),
 ]);
